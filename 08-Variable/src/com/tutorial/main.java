4c0e59e8fe495ae793938a5e6d385ccdac977f3c
novinurisnaeninovi571 <novinurisnaeni456@gmail.com>
2019-12-12 13:12:57 +0700

    java-pert-13

---
+++ 08-Variable/src/com/tutorial/main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+
+/**
+ *
+ * nama  : Faizal Rachman
+ * kelas : Ti-b
+ * nim   : A2.1900061
+ */
+public class main {
+  
+
+    public static void main(String[] args){
+
+        // kita akan membuat variabel
+        // tipe data
+        int a = 10; // assignment
+        System.out.println("nilai a = " + a);
+
+        a = 20;
+        System.out.println("nilai a baru = " + a);
+
+        // kita akan membuat sebuah deklarasi
+        int b; // deklarasi
+
+        b = 7;
+
+        System.out.println("nilai b = " + b);
+
+
+    }
+}
+ 
